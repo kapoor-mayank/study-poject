8eb04456e872dcae90cabf2bdad9dcad0edd0bb7
Mayank <mayank.kaooor@neosoftmail.com>
2024-10-09 14:34:52 +0530

    Added Logs

--- src/main/java/org/traccar/Context.java
+++ src/main/java/org/traccar/Context.java
@@ -398,9 +398,3 @@ public final class Context {
         return null;
     }
 }
-
-
-/* Location:              C:\User\\user\Documents\Ensurity Mobile [Client]\Latest App\traccar\tracker-server.jar!\org\traccar\Context.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
- */
