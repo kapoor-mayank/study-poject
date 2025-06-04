17441d25f7f73a6db6caa178a24652d885b8c01f
Mayank <mayank.kaooor@neosoftmail.com>
2025-06-04 18:32:06 +0530

    added separate key for cellcat

--- src/main/java/org/traccar/database/RedisManager.java
+++ src/main/java/org/traccar/database/RedisManager.java
@@ -29,7 +29,11 @@ public class RedisManager {
 
     public void writePosition(Position position) throws JsonProcessingException {
 //        LOGGER.info("RedisManager writePosition");
-        String key = "positions." + position.getUniqueId();
+        String key;
+        if(position.getProtocol().equals("cellcat"))
+            key = "cellcat." + position.getUniqueId();
+        else
+            key = "positions." + position.getUniqueId();
         String value = this.objectMapper.writeValueAsString(position);
         try (Jedis jedis = new Jedis(Context.getConfig().getString("redis.database"))) {
             jedis.lpush(key, new String[]{value});
